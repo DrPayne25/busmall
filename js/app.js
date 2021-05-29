4bfc700df8caa4ae852c3cb0852a3a80e7922605
DrPayne25 <alex.payne1125@gmail.com>
2021-05-29 15:49:26 -0700

    finished lab-11

--- js/app.js
+++ js/app.js
@@ -3,12 +3,13 @@
 //Global Variables
 let allProducts = [];
 let clicks = 0;
-let clicksAllowed = 15;
+let clicksAllowed = 25;
 let myContainer = document.querySelector('section');
 let myButton = document.querySelector('div');
 let imageOne = document.querySelector('section img:first-child');
 let imageTwo = document.querySelector('section img:nth-child(2)');
 let imageThree = document.querySelector('section img:nth-child(3)');
+
 
 //Object Constructor for the Products
 function Product(name, fileExtension ='jpg'){
@@ -20,14 +21,14 @@ function Product(name, fileExtension ='jpg'){
 }
 
 //Instances of the Products Object Constructor
-new Product('bad');
+new Product('bag');
 new Product('banana');
 new Product('bathroom');
 new Product('boots');
 new Product('breakfast');
 new Product('bubblegum');
 new Product('chair');
-new Product('cthlhu');
+new Product('cthulhu');
 new Product('dog-duck');
 new Product('dragon');
 new Product('pen');
@@ -45,4 +46,73 @@ function selectRandomProductIndex(){
   return Math.floor(Math.random() * allProducts.length);
 }
 
-imageOne.src ='/img/bad.jpg';
+//Function to put three products on the screen
+function renderRandomProduct(){
+  let ProductOne = selectRandomProductIndex();
+  let ProductTwo = selectRandomProductIndex();
+  let ProductThree = selectRandomProductIndex();
+  if (ProductOne === ProductTwo || ProductOne === ProductThree || ProductTwo === ProductThree){
+    return renderRandomProduct();
+  }
+
+  imageOne.src = allProducts[ProductOne].src;
+  imageOne.alt = allProducts[ProductOne].name;
+  allProducts[ProductOne].views++;
+
+  imageTwo.src = allProducts[ProductTwo].src;
+  imageTwo.alt = allProducts[ProductTwo].name;
+  allProducts[ProductTwo].views++;
+
+  imageThree.src = allProducts[ProductThree].src;
+  imageThree.alt = allProducts[ProductThree].name;
+  allProducts[ProductThree].views++;
+
+  // console.log(`
+  // Product One: ${ProductOne}
+  // Product Two: ${ProductTwo}
+  // Product Three: ${ProductThree}`);
+}
+
+//Function that will increments clicks and handles the img Click
+function handleProductClick(event){
+  if(event.target === myContainer){
+    alert('click on an IMAGE please!');
+  }
+
+  clicks++;
+  let clickProduct = event.target.alt;
+  for (let i = 0; i < allProducts.length; i++){
+    if (clickProduct === allProducts[i].name){
+      allProducts[i].clicks++;
+    }
+    renderRandomProduct();
+    if(clicks === clicksAllowed){
+      myContainer.removeEventListener('click', handleProductClick);
+      alert('You have clicked 25 times thanks for your input!');
+      return;
+    }
+  }
+}
+
+//Function to render my results to the page
+function renderResults(){
+  let ul = document.querySelector('ul');
+  for (let i = 0; i < allProducts.length; i++){
+    let li = document.createElement('li');
+    li.textContent = `${allProducts[i].name} had ${allProducts[i].views} views and was clicked ${allProducts[i].clicks} times`;
+    ul.appendChild(li);
+  }
+}
+
+//Function for allowing the View Results button
+function handleButtonClick(event){ //eslint-disable-line
+  if (clicks === clicksAllowed){
+    renderResults();
+  }
+}
+
+renderRandomProduct();
+
+//Event Listeners
+myContainer.addEventListener('click', handleProductClick);
+myButton.addEventListener('click', handleButtonClick);
